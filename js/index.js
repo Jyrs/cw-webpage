7a51c9f8716b5f9d9d13e829acf3fc7975edeed2
unknown <gj-krsnv@ya.ru>
2020-01-14 14:31:45 +0300

    index is done?

--- js/index.js
+++ js/index.js
@@ -11,13 +11,47 @@ var swiper = new Swiper('.swiper-container', {
 new WOW().init();
 
 jQuery(document).ready(function() {
+
     jQuery('.post').addClass("hid").viewportChecker({
         classToAdd: 'vis animated fadeInLeft delay-4s',
-        offset: 130
+        offset: 140
        });
 
     jQuery('.services-supp').addClass("hid").viewportChecker({
         classToAdd: 'vis animated bounceInUp delay-1s',
+        offset: 130
+        });
+
+    jQuery('.partners').addClass("hid").viewportChecker({
+        classToAdd: 'vis animated fadeIn delay-4s',
         offset: 100
         });
+
+    
+
+    $(".search-icon").click( function() {
+        if( $(".search-input").css("display") == "none")
+        {
+            $(".search-input").css("display","block");
+            $(".search-input").addClass("vis animated fadeInRight delay-1s")
+        }
+        else
+        {
+        
+            $(".search-input").hide();
+            
+        }
+    
+    });  
+
+    $(document).mouseup(function (e){
+		var div = $(".search-input");
+		if (!div.is(e.target)
+		    && div.has(e.target).length === 0) { 
+			div.hide();
+		}
+	});
+
+
 });
+
